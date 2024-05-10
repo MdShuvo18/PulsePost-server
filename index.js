ca0c9a93f2e73fb7af20db387e13713c68a22c88
MdShuvo18 <mdks33881@gmail.com>
2024-05-11 03:23:56 +0600

    add blogs done

--- index.js
+++ index.js
@@ -28,11 +28,24 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-        const blogCollection = client.db('blogCollection').collection('blogCollection');
+        const blogCollection = client.db('blogCollection').collection('blogCollection')
+
+
+
+        
+        app.get('/addBlogCollection', async (req, res) => {
+            const result = await blogCollection.find({}).toArray()
+            res.send(result)
+        })
+
+        app.post('/addBlogCollection', async (req, res) => {
+            const result = await blogCollection.insertOne(req.body)
+            res.json(result)
+        })
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
