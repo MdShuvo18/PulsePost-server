1b6f8cf17363f2c5a83d71f08749271360e54aa5
MdShuvo18 <mdks33881@gmail.com>
2024-05-11 15:42:06 +0600

    get specific blog data by id from the server successfuly

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -31,8 +31,12 @@ async function run() {
         const blogCollection = client.db('blogCollection').collection('blogCollection')
 
 
+        app.get('/blogdetails/:id', async (req, res) => {
+            const result = await blogCollection.findOne({ _id: new ObjectId(req.params.id) })
+            res.send(result)
 
-        
+        })
+
         app.get('/addBlogCollection', async (req, res) => {
             const result = await blogCollection.find({}).toArray()
             res.send(result)
@@ -45,7 +49,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
